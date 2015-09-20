2a46dffdbda21f2cb1c5b423a848e32af986bba7
Dalton A. Mitchell <daltonamitchell@live.com>
2015-09-20 02:08:39 +0000

    Display all results at index

--- dashboard/ratings/views.py
+++ dashboard/ratings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Submission
+
+from django.http import HttpResponse
+
+def index(request):
+    """Show all submissions"""
+    top_submissions_list = Submission.objects.all()
+    context = {'top_submissions_list': top_submissions_list}
+    return render(request, 'ratings/index.html', context)
